c770597bfae992c42bd70f604d859f2430bbe068
fzlink <fazlibozatay@gmail.com>
2020-03-19 18:11:36 +0300

    Cube Label

---
+++ Realm-Rush/Assets/CubeEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+
+    [SerializeField] [Range(1, 20)] private float gridSize = 10;
+
+    TextMesh textMesh;
+
+    private void Start()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+    }
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        snapPos.y = 0;
+        transform.position = snapPos;
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+    }
+}
